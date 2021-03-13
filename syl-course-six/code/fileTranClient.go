dc6fa936b152e05be12d6f2ae2a9ec8be51d98b6
scoefield <scoefield@163.com>
2021-03-13 20:38:20 +0800

    新增文件传输代码

---
+++ syl-course-six/code/fileTranClient.go
@@ -0,0 +1,89 @@
+/*
+	file: fileTranClient.go
+	runCmd: go run fileTranClient.go
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+// 发送文件内容
+func sendFileContent(conn net.Conn, fileName string) {
+	// 打开文件，默认是以只读的方式打开
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("os.Open()函数执行出错，错误信息为:%v\n", err)
+		return
+	}
+	// 延迟关闭文件句柄
+	defer file.Close()
+
+	buf := make([]byte, 4096)
+	for {
+		// 读取文件内容写到缓冲区
+		n, err := file.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("发送文件结束")
+			} else {
+				fmt.Printf("file.Read()方法执行错误,错误信息为:%v\n", err)
+			}
+			return
+		}
+		// 将文件内容发送给服务端(接收端)
+		_, err = conn.Write(buf[:n])
+	}
+}
+
+// 客户端（发送端）主函数
+func main() {
+
+	// 使用go语言的标准库os.Args获取命令行参数，返回的是字符串列表
+	args := os.Args
+
+	// 参数数量判断
+	if len(args) != 2 {
+		fmt.Printf("格式为: go run fileTranClient.go fileName\n")
+		return
+	}
+
+	// 获取文件名参数
+	filePath := args[1]
+
+	// 获取文件属性
+	fileAttr, err := os.Stat(filePath)
+	if err != nil {
+		fmt.Printf("os.Stat()函数执行出错，错误信息为:%v\n", err)
+		return
+	}
+
+	// 发起连接请求，tcp协议，端口号8080，注意这里的端口号要与服务端（接收端）的对应上
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Printf("net.Dial()函数执行出错，错误信息为:%v\n", err)
+		return
+	}
+	// 延迟关闭连接套接字
+	defer conn.Close()
+
+	// 发送文件名给服务端（接收端）
+	_, err = conn.Write([]byte(fileAttr.Name()))
+
+	// 读取服务端（接收端）回发的数据
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Printf("conn.Read()方法执行出错，错误信息为:%v\n", err)
+		return
+	}
+
+	// 如果收到服务端（接收端）的应答为"ok"，则写文件内容给服务端
+	if string(buf[:n]) == "ok" {
+		sendFileContent(conn, filePath)
+	}
+}
---
+++ syl-course-six/code/fileTranServer.go
@@ -0,0 +1,90 @@
+/*
+	file: fileTranServer.go
+	runCmd: go run fileTranServer.go
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+const (
+	MAXBUF = 4096 			// buf缓存区大小
+	TCP = "tcp"				// tcp 协议
+	ADDR = "127.0.0.1:8080"	// 监听地址
+)
+
+// 处理接收文件内容
+func recvFileContent(conn net.Conn, fileName string) {
+	// 根据文件名创建新文件
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Printf("os.Create()函数执行错误，错误信息为:%v\n", err)
+		return
+	}
+	// 延迟关闭文件句柄
+	defer file.Close()
+
+	// 循环接收客户端发过来的数据，然后写入上面创建的文件
+	for {
+		buf := make([]byte, MAXBUF)
+		n, err := conn.Read(buf)
+		// 读到的数据写入文件
+		file.Write(buf[:n])
+		if err != nil {
+			if err == io.EOF {	// io.EOF 表示读取完文件内容
+				fmt.Println("接收文件内容完成.")
+			} else {
+				fmt.Printf("conn.Read()方法执行出错，错误信息为:%v\n", err)
+			}
+			return
+		}
+	}
+}
+
+// 服务端（接收端）主函数
+func main() {
+	// 创建监听的套接字 socket，使用 tcp 协议，端口号8080
+	listener, err := net.Listen(TCP, ADDR)
+	if err != nil {
+		fmt.Printf("net.Listen()方法执行错误，错误信息为:%v\n", err)
+		return
+	}
+	// 延迟关闭监听套接字 listenner
+	defer listener.Close()
+
+	// 阻塞监听，等待客户端连接的到来
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Printf("listener.Accept()方法执行错误，错误信息为:%v\n", err)
+		return
+	}
+	// 延迟关闭连接套接字 conn
+	defer conn.Close()
+
+	// 定义一个缓冲区变量 buf，用于存储接收到的数据
+	// 此处用于保存客户端传过来的文件名
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Printf("conn.Read()方法执行错误，错误信息为:%v\n", err)
+		return
+	}
+	// 文件名前缀加"server"，重新定义为一个新文件
+	// 在本地测试时，为了区分是服务端还是客户端的文件
+	fileName := "server" + string(buf[:n])
+
+	// 接收文件名信息成功后，回写ok给客户端（发送端）
+	n, err = conn.Write([]byte("ok"))
+	if err != nil {
+		fmt.Printf("conn.Write()方法执行错误，错误信息为:%v\n", err)
+		return
+	}
+
+	// 单独封装一个函数来处理获取文件的内容
+	recvFileContent(conn, fileName)
+}
